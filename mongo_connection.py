74b745284b5f32eaad33af8aef9759c8065d2808
Tadz1k <kordas.adr@gmail.com>
2023-06-22 19:06:35 +0200

    MongoDB connection

---
+++ mongo_connection.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+
+class MongoConnection:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    def connect(self):
+        connection_string = f'mongodb://{self.host}:{self.port}/carDatabase'
+        client = MongoClient(connection_string)
+        return client['carDatabase']
+
+    def close(self):
+        self.client.close()
---
+++ mongo_utils.py
@@ -0,0 +1,15 @@
+from mongo_connection import MongoConnection
+
+# Path: mongo_utils.py
+connection = MongoConnection(host='localhost', port=27017).connect()
+collection_name = connection['cars']
+
+
+
+def insert(car):
+    car = {
+    'id': 1,
+    'name': 'Audi',
+    'price': 52642
+    }
+    collection_name.insert_one(car)
