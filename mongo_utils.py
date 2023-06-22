05859afbefd924c39eb76e83a7bb702a2b7e05ee
Tadz1k <kordas.adr@gmail.com>
2023-06-22 22:58:14 +0200

    Dynamic collection

--- mongo_utils.py
+++ mongo_utils.py
@@ -9,20 +9,20 @@ def insert(database, car):
     car_exists = None
     json_data = None
     car_id = None
+    if database == 'collection_otomoto':
+        collection = collection_otomoto
+    elif database == 'collection_olx':
+        collection = collection_olx
     try:
         json_data = json.dumps(car)
         car_id = car['id']
-        if database == 'collection_otomoto':
-            car_exists = collection_otomoto.find_one({'id': car_id})
-        elif database == 'collection_olx':
-            car_exists = collection_olx.find_one({'id': car_id})
-        print(f'car_exists: {car_exists}')
+        car_exists = collection.find_one({'id': car_id})
+
     except:
         return 0
 
     if not car_exists:
-        print('Inserting...')
-        f'{database}'.insert_one(car)
+        collection.insert_one(car)
         return 1
     else:
         return 0
