7d144bbeb61e17af011b68e00d1f52d1dbd03f5f
Tadz1k <kordas.adr@gmail.com>
2023-06-22 22:53:11 +0200

    get car id

--- mongo_utils.py
+++ mongo_utils.py
@@ -8,7 +8,6 @@ collection_olx = connection['carsAds']['olx']
 def insert(database, car):
     json_data = json.dumps(car)
     car_id = car['id']
-    print(car_id)
     car_exists = f'{database}'.find_one({'id': car_id})
     print(f'car_exists: {car_exists}')
 
