a10c934d729a5b1ade4ab0c475c7d705b6b861f3
Tadz1k <kordas.adr@gmail.com>
2023-06-22 22:23:20 +0200

    Clean tags

--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ import re
 
 def clean_tags(text):
     tags = re.findall(r'\\\\u[0-9]{1,4}[a-z A-Z]{0,3}', text)
+    tags2 = re.findall(r'\\\\n', text)
     for tag in tags:
         text = text.replace(tag, '')
+    for tag2 in tags2:
+        text = text.replace(tag2, '')
     return text
