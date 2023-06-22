1dfc52f82c428c21e9cef6c0b75a31fdd715af73
Tadz1k <kordas.adr@gmail.com>
2023-06-22 22:49:31 +0200

    Debug - print

--- mongo_utils.py
+++ mongo_utils.py
@@ -6,11 +6,12 @@ collection_otomoto = connection['carsAds']['otomoto']
 collection_olx = connection['carsAds']['olx']
 
 def insert(database, car):
+    print('Inserting v1')
     json_data = json.dumps(car)
 
     car_id = json_data.get('id')
     car_exists = f'{database}'.find_one({'id': car_id})
-    print(car_exists)
+    print(f'car_exists: {car_exists}')
 
     if not car_exists:
         print('Inserting...')
